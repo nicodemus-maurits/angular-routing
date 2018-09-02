81c7aaf44b2d2a06b2cb80dd26ed81af81c72e88
Maurits Nicodemus <nicodemus@solutiondrive.de>
2018-09-02 15:34:44 +0200

    Tambah pengecualian di route, tambah custom error message component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,8 @@ import {EditServerComponent} from './servers/edit-server/edit-server.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {Routes, RouterModule} from '@angular/router';
 import {AuthGuard} from './auth-guard.service';
+import {CanDeactivateGuard} from './servers/edit-server/can-deactivate-guard.service';
+import {ErrorPageComponent} from './error-page/error-page.component';
 
 const appRoutes: Routes = [
     {path: '', component: HomeComponent},
@@ -23,10 +25,13 @@ const appRoutes: Routes = [
         component: ServersComponent,
         children: [
             {path: ':id', component: ServerComponent},
-            {path: ':id/edit', component: EditServerComponent}
+            {path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard]}
             ]
     },
-    {path: 'not-found', component: PageNotFoundComponent},
+    // {path: 'not-found', component: PageNotFoundComponent},
+    {path: 'not-found', component: ErrorPageComponent, data: {
+        message: 'Page not found',
+    }},
     {path: '**', redirectTo: '/not-found'},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {AppRoutingModule} from './app-routing.module';
 import {AuthService} from './auth.service';
 import {AuthGuard} from './auth-guard.service';
+import {CanDeactivateGuard} from './servers/edit-server/can-deactivate-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
     declarations: [
@@ -25,7 +27,8 @@ import {AuthGuard} from './auth-guard.service';
         UserComponent,
         EditServerComponent,
         ServerComponent,
-        PageNotFoundComponent
+        PageNotFoundComponent,
+        ErrorPageComponent
     ],
     imports: [
         BrowserModule,
@@ -33,7 +36,7 @@ import {AuthGuard} from './auth-guard.service';
         HttpModule,
         AppRoutingModule,
     ],
-    providers: [ServersService, AuthService, AuthGuard],
+    providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard],
     bootstrap: [AppComponent]
 })
 export class AppModule {
